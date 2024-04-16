d4d6fcd60f8ccbc664c6b5f8cc8d1200b77b640d
haneen <haneenau4@gmail.com>
2024-04-17 00:44:08 +0300

    Main sign in and log in pages

--- ClubConnect/settings.py
+++ ClubConnect/settings.py
@@ -101,7 +101,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+AUTHENTICATION_BACKENDS = [
+    'pages.authentication_backends.EmailAuthBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -126,3 +129,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT= os.path.join(BASE_DIR,'media')
